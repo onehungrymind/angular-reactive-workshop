8ae0294e06b53c0756ecaf50da99489ee653058c
Lukas Ruebbelke <simpul@gmail.com>
2018-10-24 09:23:50 -0500

    update: creating helper functions

---
+++ libs/core-data/src/lib/state/projects/projects.reducer.ts
@@ -0,0 +1,34 @@
+import { Project } from './../../projects/project.model';
+
+const initialProjects: Project[] = [
+  {
+    id: '1',
+    title: 'Project One',
+    details: 'This is a sample project',
+    percentComplete: 20,
+    approved: false,
+    customerId: null
+  },
+  {
+    id: '2',
+    title: 'Project Two',
+    details: 'This is a sample project',
+    percentComplete: 40,
+    approved: false,
+    customerId: null
+  },
+  {
+    id: '3',
+    title: 'Project Three',
+    details: 'This is a sample project',
+    percentComplete: 100,
+    approved: true,
+    customerId: null
+  }
+];
+
+const createProject = (projects, project) => [...projects, project];
+const updateProject = (projects, project) => projects.map(p => {
+  return p.id === project.id ? Object.assign({}, project) : p;
+});
+const deleteProject = (projects, project) => projects.filter(w => project.id !== w.id);

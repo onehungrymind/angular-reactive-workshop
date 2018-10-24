c716fd84e6ef92abdad31c7867cfa78c82de871a
Lukas Ruebbelke <simpul@gmail.com>
2018-10-24 06:56:16 -0500

    update: cleaning up

--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -1,14 +1,12 @@
-import { AuthGuardService } from './auth/auth-guard.service';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { AuthGuardService } from './auth/auth-guard.service';
 import { AuthService } from './auth/auth.service';
-import { TokenInterceptor } from './auth/token.interceptor';
+import { CustomersService } from './customers/customers.service';
 import { NotificationsService } from './notifications/notifications.service';
 import { ProjectsService } from './projects/projects.service';
-import { ErrorInterceptor } from './error/error.interceptor';
-import { CustomersService } from './customers/customers.service';
 
 @NgModule({
   providers: [
@@ -16,17 +14,7 @@ import { CustomersService } from './customers/customers.service';
     AuthGuardService,
     NotificationsService,
     CustomersService,
-    ProjectsService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true
-    }
+    ProjectsService
   ],
   imports: [CommonModule, HttpClientModule],
 })

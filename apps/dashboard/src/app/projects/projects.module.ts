ab3470a7f836512ebda12927bbd00516d192f163
Victor Avila <victor.avila@venturplex.com>
2019-10-29 14:31:41 -0700

    update: projects module

--- apps/dashboard/src/app/projects/projects.module.ts
+++ apps/dashboard/src/app/projects/projects.module.ts
@@ -7,7 +7,6 @@ import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { MaterialModule } from '@workshop/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CoreDataModule } from '@workshop/core-data';
 
 @NgModule({
   imports: [
@@ -15,8 +14,7 @@ import { CoreDataModule } from '@workshop/core-data';
     ProjectsRoutingModule,
     MaterialModule,
     ReactiveFormsModule,
-    FormsModule,
-    CoreDataModule
+    FormsModule
   ],
   declarations: [
     ProjectsComponent,
